77dc2585e1ae22f87bce2f08a6cb03beded323b7
grofzero <grofzero@hotmail.com>
2021-06-19 08:56:27 +0200

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,9 +1,29 @@
 function go_straight () {
     cuteBot.motors(43, 45)
 }
-input.onButtonPressed(Button.A, function () {
-    go_straight()
+input.onSound(DetectedSound.Loud, function () {
+    fut = !(fut)
 })
+let fut = false
+fut = false
 basic.forever(function () {
-	
+    if (fut) {
+        go_straight()
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        cuteBot.motors(0, 0)
+        basic.showLeds(`
+            . # # . .
+            # . . # .
+            # . . # .
+            # . . # .
+            . # # . .
+            `)
+    }
 })
